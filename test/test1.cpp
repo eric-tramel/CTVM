1a89490e91fa087c6bf2889de41bf6c8ee538823
Eric W. Tramel <eric.tramel@gmail.com>
2015-07-30 13:13:54 +0200

    Test1 executable now needs file names
    Specified the need for filenames to be passed to the Test1 executable.
    This is to account for the discrepancies between the Windows and Mac
    build versions. If no file names are passed, the ImageMagick tests are
    skipped.

--- test/test1.cpp
+++ test/test1.cpp
@@ -44,42 +44,47 @@ int main(int argc, char **argv)
 	std::cout << "Normalized: " << NormalizeMatrix(TestMatrix) << std::endl;
 
 	/* Test ImageMagick */
-	std::cout << std::endl;
-	using namespace Magick;
-	InitializeMagick("");
+	// Only run ImageMagick stuff if there are file names specified
+	if(argc > 2){
+		char* test_image = argv[1];
+		char* output_image = argv[2];
+		std::cout << std::endl;
+		using namespace Magick;
+		InitializeMagick("");
 
-	std::cout << "Testing ImageMagick Link." << std::endl;
-	Image someImage;
-	someImage.read("C:\\data\\peppers.jpg");
-	std::cout << "    " << "Passed." << std::endl;
+		std::cout << "Testing ImageMagick Link." << std::endl;
+		Image someImage;
+		someImage.read(test_image);
+		std::cout << "    " << "Passed." << std::endl;
 
-	std::cout << "Creating Novel Image" << std::endl;
-	Image anotherImage;
-	anotherImage.size(Geometry(32, 32));         // Specify the dimensionality
-	Pixels anotherImageView(anotherImage);      // Get a view of the image
-	PixelPacket aPixel;
-	aPixel.red = ColorGray::scaleDoubleToQuantum(0.5);
-	aPixel.green = ColorGray::scaleDoubleToQuantum(0.5);
-	aPixel.blue = ColorGray::scaleDoubleToQuantum(0.5);
-	*(anotherImageView.get(16, 16, 1, 1)) = aPixel;
-	anotherImage.type(GrayscaleType);           // Specify the color type
-	anotherImageView.sync();
-	anotherImage.write("C:\\data\\testoutimage.png");
-	std::cout << "    " << "Passed." << std::endl;
+		std::cout << "Creating Novel Image" << std::endl;
+		Image anotherImage;
+		anotherImage.size(Geometry(32, 32));         // Specify the dimensionality
+		Pixels anotherImageView(anotherImage);      // Get a view of the image
+		PixelPacket aPixel;
+		aPixel.red = ColorGray::scaleDoubleToQuantum(0.5);
+		aPixel.green = ColorGray::scaleDoubleToQuantum(0.5);
+		aPixel.blue = ColorGray::scaleDoubleToQuantum(0.5);
+		*(anotherImageView.get(16, 16, 1, 1)) = aPixel;
+		anotherImage.type(GrayscaleType);           // Specify the color type
+		anotherImageView.sync();
+		anotherImage.write(output_image);
+		std::cout << "    " << "Passed." << std::endl;
 
-	/* Test CTVM Image Load */
-	std::cout << std::endl;
-	std::cout << "Testing CTVM Image Load." << std::endl;
-	BoostDoubleMatrix ImageMatrix = LoadImage("C:\\data\\peppers.jpg");
-	std::cout << "Image Data:" << std::endl;
-	std::cout << ImageMatrix(0, 0) << " " << ImageMatrix(0, 1) << " " << ImageMatrix(0, 3) << std::endl;
-	std::cout << ImageMatrix(1, 0) << " " << ImageMatrix(1, 1) << " " << ImageMatrix(1, 3) << std::endl;
-	std::cout << ImageMatrix(2, 0) << " " << ImageMatrix(2, 1) << " " << ImageMatrix(2, 3) << std::endl;
-	std::cout << "    " << "Passed." << std::endl;
+		/* Test CTVM Image Load */
+		std::cout << std::endl;
+		std::cout << "Testing CTVM Image Load." << std::endl;
+		BoostDoubleMatrix ImageMatrix = LoadImage(test_image);
+		std::cout << "Image Data:" << std::endl;
+		std::cout << ImageMatrix(0, 0) << " " << ImageMatrix(0, 1) << " " << ImageMatrix(0, 3) << std::endl;
+		std::cout << ImageMatrix(1, 0) << " " << ImageMatrix(1, 1) << " " << ImageMatrix(1, 3) << std::endl;
+		std::cout << ImageMatrix(2, 0) << " " << ImageMatrix(2, 1) << " " << ImageMatrix(2, 3) << std::endl;
+		std::cout << "    " << "Passed." << std::endl;
 
-	std::cout << "Testing CTVM Image Write." << std::endl;
-	WriteImage(ImageMatrix, "C:\\data\\test_peppers.jpg");
-	std::cout << "    " << "Passed." << std::endl;
+		std::cout << "Testing CTVM Image Write." << std::endl;
+		WriteImage(ImageMatrix, output_image);
+		std::cout << "    " << "Passed." << std::endl;
+	}
 
 	/* Initialisation */
 	BoostDoubleMatrix A(2, 4), W(4, 2), NU(4, 2);
